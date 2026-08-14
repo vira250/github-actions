10a2b72f5396c03f2afe3c4d8ce52c745838ad0e
vira250 <jviraj666@gmail.com>
2026-08-14 12:28:25 +0530

    controller class deleted

--- src/main/java/com/demo/cicd_github_Actions/CicdGithubActionsApplication.java
+++ src/main/java/com/demo/cicd_github_Actions/CicdGithubActionsApplication.java
@@ -2,10 +2,17 @@ package com.demo.cicd_github_Actions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdGithubActionsApplication {
 
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Hello India";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CicdGithubActionsApplication.class, args);
 	}
