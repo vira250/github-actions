b404884ede9ffd61b8f9e523ef08027119bf5bde
vira250 <jviraj666@gmail.com>
2026-09-02 14:45:23 +0530

    testing agent

--- src/main/java/com/demo/cicd_github_Actions/CicdGithubActionsApplication.java
+++ src/main/java/com/demo/cicd_github_Actions/CicdGithubActionsApplication.java
@@ -11,7 +11,7 @@ public class CicdGithubActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Hello Terna";
+		return "Hello ALL";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(CicdGithubActionsApplication.class, args);
